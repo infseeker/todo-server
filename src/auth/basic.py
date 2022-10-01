bb3419ad7c47602f7124202c6f392e06e0aeda7f
infseeker <infseek@gmail.com>
2022-10-01 21:40:16 +0200

    config: add react domain to cors

--- src/auth/basic.py
+++ src/auth/basic.py
@@ -37,6 +37,7 @@ cors = CORS(
                 'http://192.168.0.2:3000',
                 'http://127.0.0.1:3000',
                 'http://localhost:3000',
+                'https://react.infseeker-todo.tk'
             ]
         }
     },
